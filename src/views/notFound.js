da9e016accdacfd388daa05ee5c186d948321131
Webster Fievre <108379057+WebsterFever@users.noreply.github.com>
2026-09-19 21:04:43 -0400

    Translate src/views/notFound.js to English

--- src/views/notFound.js
+++ src/views/notFound.js
@@ -2,8 +2,8 @@ export function renderNotFound(container) {
   container.innerHTML = `
     <section class="not-found">
       <h1>404</h1>
-      <p>La pagina que buscas no existe.</p>
-      <a href="/home" data-link class="btn-primary">Volver al inicio</a>
+      <p>The page you are looking for does not exist.</p>
+      <a href="/home" data-link class="btn-primary">Back to home</a>
     </section>
   `;
 }
